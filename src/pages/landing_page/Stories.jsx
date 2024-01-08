b89d4b3af2b049a530b7b52f5ab632cab90d77b3
codewithakky <akkyjaicar@gmail.com>
2024-01-08 17:35:59 +0530

    UI fixed for mobile

--- src/pages/landing_page/Stories.jsx
+++ src/pages/landing_page/Stories.jsx
@@ -2,7 +2,7 @@ import StoriesModal from "../../components/StoriesModal";
 
 function Stories() {
   return (
-    <div className="flex w-screen h-screen overflow-hidden bg-black">
+    <div className="flex w-screen h-svh overflow-hidden bg-black">
       <div className="flex w-full justify-center">
         <div className="flex flex-col bg-white max-sm:w-full w-[400px]">
           <StoriesModal />
