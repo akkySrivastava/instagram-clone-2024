184fcc8dfaf399df8b6245f5f512ec09824f8ed9
codewithakky <akkyjaicar@gmail.com>
2024-01-08 18:02:07 +0530

    UI fixed for mobile

--- src/pages/landing_page/Reels.jsx
+++ src/pages/landing_page/Reels.jsx
@@ -3,7 +3,7 @@ import ReelsModal from "../../components/ReelsModal";
 
 function Reels() {
   return (
-    <div className="flex w-screen h-h-svh bg-black">
+    <div className="flex w-screen h-svh bg-black">
       <div className="flex w-full justify-center">
         <div className="flex flex-col bg-white max-sm:w-full w-[400px]">
           <ReelsModal />
